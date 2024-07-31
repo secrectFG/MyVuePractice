2c96f319e6726dccbb53d37457fda672b6ded3d9
secrect <2724652+secrectFG@users.noreply.github.com>
2024-07-31 19:33:44 +0800

    登录静态界面

--- vue3.2/.eslintrc.js
+++ vue3.2/.eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single'], // 使用单引号
+    semi: ['error', 'never'], // 禁止使用分号
+    'comma-dangle': ['error', 'never'] // 禁止使用尾随逗号
   }
 }
